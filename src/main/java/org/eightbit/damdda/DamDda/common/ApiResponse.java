d99756f8bddf004d31be07a52e9f1d1852f2facb
hwangjiyoung02 <hjy020714@gmail.com>
2025-01-28 20:00:04 +0900

    feat: springsecurity

--- src/main/java/org/eightbit/damdda/DamDda/common/ApiResponse.java
+++ src/main/java/org/eightbit/damdda/DamDda/common/ApiResponse.java
@@ -1,8 +1,6 @@
 package org.eightbit.damdda.DamDda.common;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AccessLevel;
-import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
