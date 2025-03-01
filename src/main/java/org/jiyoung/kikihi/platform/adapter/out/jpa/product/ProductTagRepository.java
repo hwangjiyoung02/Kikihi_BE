a07ae14a7516ec2c1448c98651c9960b3298136b
eedo_y <doup2001@naver.com>
2025-03-02 01:56:50 +0900

    🔨chore : 필요없는 내용 삭제 - sql, 인증/인가 부분 제거 - properties 제거

--- src/main/java/org/jiyoung/kikihi/platform/adapter/out/jpa/product/ProductTagRepository.java
+++
@@ -1,9 +0,0 @@
-package org.jiyoung.kikihi.platform.adapter.out.jpa.product;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductTagRepository extends JpaRepository<ProductTag, Long> {
-}
--- src/main/java/org/jiyoung/kikihi/platform/adapter/out/jpa/user/User.java
+++ src/main/java/org/jiyoung/kikihi/platform/adapter/out/jpa/user/User.java
@@ -2,12 +2,6 @@ package org.jiyoung.kikihi.platform.adapter.out.jpa.user;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -15,7 +9,7 @@ import java.util.List;
 @AllArgsConstructor
 @Getter
 @Builder
-public class User implements UserDetails {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
@@ -36,37 +30,6 @@ public class User implements UserDetails {
     @Setter
     @Column(nullable = true)
     private String userName;
-
-    //권한 반환
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities(){
-        return List.of(new SimpleGrantedAuthority("user"));
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override// 만료되었는지 확인하는 로직
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override// 계정 잠금 여부 반환
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override// 패스워드 만료 여부 반환
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override//계정 사용 가능 여부 반환
-    public boolean isEnabled() {
-        return true;
-    }
 
     //생성자
     @Builder
